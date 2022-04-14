5dfc2e00ece97a9a21c90061a1d374c8d7f99c68
coolabhays <abhaysp9955@gmail.com>
2022-04-14 17:36:29 +0530

    feat: helper functions for fswalk

---
+++ fswalk/actions.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+func filterOut(path, ext string, minSize uint64, info os.FileInfo) bool {
+	if info.IsDir() || info.Size() < int64(minSize) {
+		return true
+	}
+
+	if ext != "" && filepath.Ext(path) != ext {
+		return true
+	}
+
+	return false
+}
+
+func listFile(path string, out io.Writer) error {
+	_, err := fmt.Fprintln(out, path)
+	return err
+}
