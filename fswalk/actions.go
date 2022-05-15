9e4cedf878201c950e698a6bb8202d249bdde358
coolabhays <abhaysp9955@gmail.com>
2022-05-15 13:48:08 +0530

    feat: add solutions for the exercises
        can provide more extensions to filterout
        can provide date to list to filterout files modified before
        can now decompress the the files preserving dentry

--- fswalk/actions.go
+++ fswalk/actions.go
@@ -7,15 +7,35 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
+	"time"
 )
 
-func filterOut(path, ext string, minSize uint64, info os.FileInfo) bool {
+var (
+	DLayout = "2006-01-02"
+)
+
+func filterOut(path, ext, mDate string, minSize uint64, info os.FileInfo) bool {
 	if info.IsDir() || info.Size() < int64(minSize) {
 		return true
 	}
 
-	if ext != "" && filepath.Ext(path) != ext {
-		return true
+	if mDate != "" {
+		d, err := time.Parse(DLayout,mDate);
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		if info.ModTime().Before(d) {
+			return true
+		}
+	}
+
+	exts := strings.Split(ext, ",")
+	for _, ext := range exts {
+		if filepath.Ext(path) == ext {
+			 return true
+		}
 	}
 
 	return false
@@ -69,7 +89,12 @@ func archiveFile(destDir, root, path string) error {
 	}
 	defer in.Close()
 
-	zw := gzip.NewWriter(out)
+	// zw := gzip.NewWriter(out)
+	zw, err := gzip.NewWriterLevel(out, 9)
+	if err != nil {
+		return err
+	}
+
 	if _, err := io.Copy(zw, in); err != nil {
 		return err
 	}
@@ -79,4 +104,51 @@ func archiveFile(destDir, root, path string) error {
 	}
 
 	return nil
+}
+
+func unarchiveFile(destDir, root, path string) error {
+	// do the checks
+	info, err := os.Stat(destDir)
+	if err != nil {
+		return err
+	}
+	if !info.IsDir() {
+		return fmt.Errorf("%q is not a directory", destDir)
+	}
+	relDir, err := filepath.Rel(root, filepath.Dir(path))
+	if err != nil {
+		return err
+	}
+	if filepath.Ext(path) != ".gz" {
+		return fmt.Errorf("%q is not a archive file", path)
+	}
+
+	// prepare paths
+	dest := filepath.Base(strings.TrimSuffix(path, filepath.Ext(path)))
+	targetPath := filepath.Join(destDir, relDir, dest)
+	if err := os.MkdirAll(filepath.Dir(targetPath), 0755); err != nil {
+		return err
+	}
+
+	// open files
+	out, err := os.OpenFile(targetPath, os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	in, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	// start decompressing
+	zR, err := gzip.NewReader(in)
+	if err != nil {
+		return err
+	}
+	if _, err := io.Copy(out, zR); err != nil {
+		return err
+	}
+	return zR.Close()
 }
--- fswalk/main.go
+++ fswalk/main.go
@@ -16,16 +16,22 @@ type config struct {
 	del bool  // delete files
 	wLog io.Writer  // to log
 	archive string  // archive directory
+	mDate string  // specify date
+	unarchive bool  // if you want to unarchive
 }
 
 func main() {
 	root := flag.String("root", ".", "Root directory to start")
 	list := flag.Bool("list", false, "List files only")
-	ext := flag.String("ext", "", "File extension to filter out")
+	ext := flag.String("ext", "", "File extension to filter out (.ext1,.ext2 etc.)")
 	size := flag.Uint64("size", 0, "Minimum file size")
 	del := flag.Bool("del", false, "Delete files")
 	logFile := flag.String("log", "", "Log deletes to the file")
 	archive := flag.String("archive", "", "Archive directory")
+	date := flag.String("date", "1901-01-01", `List files modified from and onwards the mentioned date
+	(format: 2006-02-01)`)
+	unarchive := flag.Bool("uarc", false, `set true if want to unarchive,
+	set folder from -archive flag`)
 	flag.Parse()
 
 	var (
@@ -49,6 +55,8 @@ func main() {
 		del: *del,
 		wLog: f,
 		archive: *archive,
+		mDate: *date,
+		unarchive: *unarchive,
 	}
 
 	if err := run(*root, os.Stdout, c); err != nil {
@@ -66,7 +74,7 @@ func run(root string, out io.Writer, cfg config) error {
 				return err
 			}
 
-			if filterOut(path, cfg.ext, cfg.size, info) {
+			if filterOut(path, cfg.ext, cfg.mDate, cfg.size, info) {
 				return nil
 			}
 
@@ -77,6 +85,17 @@ func run(root string, out io.Writer, cfg config) error {
 
 			if cfg.del {
 				return delFile(path, delLogger)
+			}
+
+			if cfg.unarchive {
+				fmt.Println(cfg.archive)
+				if cfg.archive != "" {
+					if err := listFile(path, out); err != nil {
+						return err
+					}
+					return unarchiveFile(cfg.archive, root, path)
+				}
+				return fmt.Errorf("destination not provided")
 			}
 
 			if cfg.archive != "" {
