700af96fd78ef05f1ac2c12bee4f9f0b061645e0
coolabhays <abhaysp9955@gmail.com>
2022-05-26 15:28:59 +0530

    feat: add test for "min" and "max" operation

--- colstats/csv_test.go
+++ colstats/csv_test.go
@@ -24,6 +24,8 @@ func TestOperations(t *testing.T) {
 	} {
 		{"sum", sum, []float64{300, 85.927, -30, 436}},
 		{"avg", avg, []float64{37.5, 6.609769230769231, -15, 72.666666666666666}},
+		{"min", min, []float64{10, 2.2, -20, 37}},
+		{"max", max, []float64{100, 12.287, -10, 129}},
 	}
 
 	for _, tc := range testCases {
