a6ce64f910b081b9b7f72ed8085d77f8f42776ff
coolabhays <abhaysp9955@gmail.com>
2022-05-26 08:41:08 +0530

    feat: add benchmarking

--- colstats/main_test.go
+++ colstats/main_test.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"io"
+	"path/filepath"
 
 	// "os"
 	"testing"
@@ -97,4 +99,19 @@ func TestRun(t *testing.T) {
 			}
 		})
 	}
+}
+
+func BenchmarkRun(b *testing.B) {
+	filenames, err := filepath.Glob("./testdata/*.csv")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if err := run(filenames, "avg", 2, io.Discard); err != nil {
+			b.Error(err)
+		}
+	}
+
 }
