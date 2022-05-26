df814bfede6deeed356a1ffc0c27189448b1f7c0
coolabhays <abhaysp9955@gmail.com>
2022-05-26 15:29:27 +0530

    feat: add Benchmark for max operation

--- colstats/main_test.go
+++ colstats/main_test.go
@@ -115,3 +115,17 @@ func BenchmarkRun(b *testing.B) {
 	}
 
 }
+
+func BenchmarkMaxRun(b *testing.B) {
+	filenames, err := filepath.Glob("./testdata/*.csv")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if err := run(filenames, "max", 2, io.Discard); err != nil {
+			b.Error(err)
+		}
+	}
+}
