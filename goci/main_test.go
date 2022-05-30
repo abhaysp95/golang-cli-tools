1246658a0d476be12266d8748b34c5b89adc845d
coolabhays <abhaysp9955@gmail.com>
2022-05-30 12:12:46 +0530

    feat: unit testing for running a pipeline command

---
+++ goci/main_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"testing"
+)
+
+func TestRun(t *testing.T) {
+	testCases := []struct {
+		name   string
+		proj   string
+		out    string
+		expErr error
+	}{
+		{
+			name:   "success",
+			proj:   "./testdata/tool",
+			out:    "Go Build: SUCCESS\nGo Test: SUCCESS\n",
+			expErr: nil,
+		},
+		{
+			name:   "fail",
+			proj:   "./testdata/toolErr",
+			out:    "",
+			expErr: &stepErr{step: "go build"},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var out bytes.Buffer
+
+			err := run(tc.proj, &out)
+
+			if tc.expErr != nil {
+				if err == nil {
+					t.Errorf("Expected %q, got 'nil' instead", tc.expErr)
+				}
+
+				/* if _, ok := tc.expErr.(*stepErr); ok {  // just for testing purpose (safe to remove)
+					t.Errorf("type match")
+				} */
+
+				if !errors.Is(err, tc.expErr) {  // <-- looks like custom Is() is not getting called
+				// if errors.Is(errors.Unwrap(err), tc.expErr) {  // this is working
+					t.Errorf("Expected error: %q. Got: %q.", tc.expErr, err)
+				}
+				return
+			}
+			if err != nil {
+				t.Errorf("Unexpected err: %q", err)
+			}
+			if out.String() != tc.out {
+				t.Errorf("Expected output: %q. Got: %q", tc.out, out.String())
+			}
+		})
+	}
+}
