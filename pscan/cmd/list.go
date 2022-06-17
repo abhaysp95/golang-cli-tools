7f302d0f4df46f1921d72ec566cc518dca7d9318
coolabhays <abhaysp9955@gmail.com>
2022-06-17 17:09:52 +0530

    feat: get hosts file by viper

--- pscan/cmd/list.go
+++ pscan/cmd/list.go
@@ -28,6 +28,7 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // listCmd represents the list command
@@ -36,10 +37,8 @@ var listCmd = &cobra.Command{
 	Use:     "list",
 	Short:   "List hosts in hosts list",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		hostsFile, err := cmd.Flags().GetString("hosts-file")
-		if err != nil {
-			return err
-		}
+		hostsFile := viper.GetString("hosts-file")
+
 		return listAction(os.Stdout, hostsFile, args)
 	},
 }
